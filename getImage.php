e2a84310d0899edbe2c844ea867779fbaf7fbb04
Peter Melchior <peter.m.melchior@gmail.com>
2013-11-12 03:04:22 +0000

    added FoV image (closes #19) and eyeball squad report (closes #29)

--- getImage.php
+++ getImage.php
@@ -2,12 +2,10 @@
 function download_file($file) { // $file = include path 
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-        //header('Content-Disposition: attachment; filename='.basename($file));
         header('Content-Transfer-Encoding: binary');
         header('Expires: 0');
         header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
         header('Pragma: public');
-        //header('Content-Length: ' . filesize($file));
         ob_clean();
         flush();
         readfile($file);

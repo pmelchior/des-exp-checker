67cc18f6c98cf2f204f28ccff14a95e55f41e09b
Peter Melchior <peter.m.melchior@gmail.com>
2013-12-01 07:30:44 +0000

    changed to server-based congrats; advent calender

--- db.php
+++ db.php
@@ -29,7 +29,7 @@ if ($_POST) {
             $sth2 = $dbh->prepare('UPDATE users SET total_files = total_files + 1 WHERE rowid = ?');
             $sth2->execute(array($uid));
         }
-        //$activity = getActivity($dbh, $uid);
+        $activity = getActivity($dbh, $uid);
     }
 }
 
@@ -37,9 +37,40 @@ if ($_POST) {
 $res = getNextImage($dbh);
 $row = $res->fetch(PDO::FETCH_ASSOC);
 $row['name'] = "getImage.php?name=".$row['name'];
-/*if ($_POST)
-    $row['activity'] = $activity;
-*/
+
+
+if ($_POST) {
+    // badge of honor:
+    $uc = userClass($activity['alltime']);
+    $old_uc = userClass($activity['alltime']-1);
+    if ($uc > $old_uc) {
+        $row['congrats'] = array('text'=> "You have just finished your ",
+                                'detail' => "To reflect your achievements, we've upgraded you to <span id='status_class' class='badge'></span> status.",
+                                'userclass' => $uc
+                                );
+        if($uc == 1)
+            $row['congrats']['text'] .= "<strong>first 10 images</strong>!";
+        else {
+            $fps = $activity['alltime'] / $config['images_per_fp'];
+            $row['congrats']['text'] .= "<strong>". numberSuffix($fps). " focal plane</strong>!";
+        }
+    }
+    elseif ($activity['alltime'] % $config['images_per_fp'] == 0) {
+        $fps = $activity['alltime'] / $config['images_per_fp'];
+        $row['congrats'] = array('text'=> "You have just finished your <strong>". numberSuffix($fps). " focal plane</strong>!");
+    }
+    
+    // advent calendar
+    if ($activity['today'] == 10) {
+        giveBonusPoints($dbh, $uid, 10);
+        if (isset($row['congrats'])) {
+            $row['congrats']['detail'] .= "<br>You have also just finished your <strong>10th image today</strong>. And since it is Advent, and Advent is the time to be generous, we give you <span class='label label-important'>+10</span> points to your account.";
+        } else 
+            $row['congrats'] = array('text'=> "You have just finished your <strong>10th image today</strong>!",
+                                    'detail' => "It is Advent. And since Advent is the time to be generous, we give you <span class='label label-important'>+10</span> points to your account."
+                                    );
+    }
+}
 echo json_encode($row);
 
 ?>

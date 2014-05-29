86cec1ed5a35e5c84ee03e374142bd509dc7d8e9
Peter Melchior <peter.m.melchior@gmail.com>
2014-05-29 23:41:54 +0000

    improved stats reporting

--- stats.php
+++ stats.php
@@ -6,17 +6,18 @@ $dbh = getDBHandle();
 $stats = array();
 // basic stats: how many files done
 if (isset($_GET['total'])) {
-    $stmt = $dbh->query('SELECT COUNT(DISTINCT(fileid)) FROM qa');
+    // to the degree that submission come in slower than 1/second, this is correct
+    $stmt = $dbh->query('SELECT COUNT(DISTINCT(timestamp)) FROM qa');
     $stats['total'] = intval($stmt->fetchColumn());
 }
 if (isset($_GET['today'])) {
     $date = date('Y-m-d H:i:s', strtotime('-1 day'));
-    $stmt = $dbh->query("SELECT COUNT(DISTINCT(fileid)) FROM qa WHERE timestamp > '".$date."%'");
+    $stmt = $dbh->query("SELECT COUNT(DISTINCT(timestamp)) FROM qa WHERE timestamp > '".$date."%'");
     $stats['today'] = intval($stmt->fetchColumn());
 }
 if (isset($_GET['breakup'])) {
     // how many have problems
-    $stmt = $dbh->prepare('SELECT COUNT(DISTINCT(fileid)), COUNT(fileid) from qa WHERE problem = ?');
+    $stmt = $dbh->prepare('SELECT COUNT(DISTINCT(fileid)) from qa WHERE problem = ?');
     $codes = getProblemCodes();
     $problems = array();
     $stats['checked'] = 0;
@@ -29,7 +30,7 @@ if (isset($_GET['breakup'])) {
             if ($code == 0)
                 $stats['fine'] = intval($row[0]);
             else
-                array_push($problems, array("name" => $name, "distinct" => intval($row[0]), "all" => intval($row[1])));
+                array_push($problems, array("name" => $name, "distinct" => intval($row[0])));
         }
     }
     $stats['breakup'] = $problems;

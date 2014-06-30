76923a1158779b07e91118f0e837902db65fcf91
Peter Melchior <peter.m.melchior@gmail.com>
2014-06-30 20:27:13 +0000

    first stab at #54, but no optimal ordering yet

--- db.php
+++ db.php
@@ -2,63 +2,61 @@
 
 include "common.php.inc";
 $dbh = getDBHandle();
+$uid = getUIDFromSID($dbh);
 
 // check if problem marks are present
-if (isset($_POST['fileid'])) {
-    $uid = getUIDFromSID($dbh);
-    if ($uid && $_POST['fileid'] != '') {
-        // parse POST data and store each element in table qa
-        $sth = $dbh->prepare('INSERT INTO qa (fileid, userid, problem, x, y, detail) VALUES (?, ?, ?, ?, ?, ?)');
-        if (isset($_POST['problems'])) {
-            $codes = getProblemCodes();
-            foreach ($_POST['problems'] as $problem) {
-                if ($problem['problem'][0] == "-") {
-                    $problem['problem'] = substr($problem['problem'], 1);
-                    $code = -$codes[$problem['problem']];
-                }
-                else 
-                    $code = $codes[$problem['problem']];
-                $problem['x'] = (int) $problem['x'];
-                $problem['y'] = (int) $problem['y'];
-                if ($problem['detail'] == '')
-                   $problem['detail'] = null;
-                // stores x,y, and (occasionally a free-form comment)
-                $sth->execute(array($_POST['fileid'], $uid, $code, $problem['x'], $problem['y'], $problem['detail']));
+if ($uid && isset($_POST['fileid']) && $_POST['fileid'] != '') {
+    // parse POST data and store each element in table qa
+    $sth = $dbh->prepare('INSERT INTO qa (fileid, userid, problem, x, y, detail) VALUES (?, ?, ?, ?, ?, ?)');
+    if (isset($_POST['problems'])) {
+        $codes = getProblemCodes();
+        foreach ($_POST['problems'] as $problem) {
+            if ($problem['problem'][0] == "-") {
+                $problem['problem'] = substr($problem['problem'], 1);
+                $code = -$codes[$problem['problem']];
             }
-            // update attached user database to reflect user action
-            $sth2 = $dbh->prepare('UPDATE users SET total_files = total_files + 1, flagged_files = flagged_files + 1 WHERE rowid = ?');
-            $sth2->execute(array($uid));
-        } else {
-            $sth->execute(array($_POST['fileid'], $uid, 0, null, null, null));
-            $sth2 = $dbh->prepare('UPDATE users SET total_files = total_files + 1 WHERE rowid = ?');
-            $sth2->execute(array($uid));
+            else 
+                $code = $codes[$problem['problem']];
+            $problem['x'] = (int) $problem['x'];
+            $problem['y'] = (int) $problem['y'];
+            if ($problem['detail'] == '')
+               $problem['detail'] = null;
+            // stores x,y, and (occasionally a free-form comment)
+            $sth->execute(array($_POST['fileid'], $uid, $code, $problem['x'], $problem['y'], $problem['detail']));
         }
-        $activity = getActivity($dbh, $uid);
-        
-        // badge of honor:
-        $uc = userClass($activity['alltime']);
-        $old_uc = userClass($activity['alltime']-1);
-        if ($uc > $old_uc) {
-            $congrats = array('text'=> "You have just finished your ",
-                                    'detail' => "To reflect your achievements, we've upgraded you to <span id='status_class' class='badge'></span> status.",
-                                    'userclass' => $uc
-                                    );
-            if($uc == 1)
-                $congrats['text'] .= "<strong>first 10 images</strong>!";
-            else {
-                $fps = $activity['alltime'] / $config['images_per_fp'];
-                $congrats['text'] .= "<strong>". numberSuffix($fps). " focal plane</strong>!";
-            }
-        }
-        elseif ($activity['alltime'] % $config['images_per_fp'] == 0) {
+        // update attached user database to reflect user action
+        $sth2 = $dbh->prepare('UPDATE users SET total_files = total_files + 1, flagged_files = flagged_files + 1 WHERE rowid = ?');
+        $sth2->execute(array($uid));
+    } else {
+        $sth->execute(array($_POST['fileid'], $uid, 0, null, null, null));
+        $sth2 = $dbh->prepare('UPDATE users SET total_files = total_files + 1 WHERE rowid = ?');
+        $sth2->execute(array($uid));
+    }
+    $activity = getActivity($dbh, $uid);
+    
+    // badge of honor:
+    $uc = userClass($activity['alltime']);
+    $old_uc = userClass($activity['alltime']-1);
+    if ($uc > $old_uc) {
+        $congrats = array('text'=> "You have just finished your ",
+                                'detail' => "To reflect your achievements, we've upgraded you to <span id='status_class' class='badge'></span> status.",
+                                'userclass' => $uc
+                                );
+        if($uc == 1)
+            $congrats['text'] .= "<strong>first 10 images</strong>!";
+        else {
             $fps = $activity['alltime'] / $config['images_per_fp'];
-            $congrats = array('text'=> "You have just finished your <strong>". numberSuffix($fps). " focal plane</strong>!");
+            $congrats['text'] .= "<strong>". numberSuffix($fps). " focal plane</strong>!";
         }
+    }
+    elseif ($activity['alltime'] % $config['images_per_fp'] == 0) {
+        $fps = $activity['alltime'] / $config['images_per_fp'];
+        $congrats = array('text'=> "You have just finished your <strong>". numberSuffix($fps). " focal plane</strong>!");
     }
 }
 
 // return the next image
-$row = getNextImage($dbh);
+$row = getNextImage($dbh, $uid);
 if ($row) {
     $row['name'] = "getImage.php?name=".$row['name'];
     // problem marks are requested

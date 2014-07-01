51ea29a74064665dfc0850a80987621a916b540f
Peter Melchior <peter.m.melchior@gmail.com>
2014-07-01 17:41:14 +0000

    re-organized sql files

---
+++ sql/files.sql
@@ -0,0 +1,29 @@
+-- files database structure
+CREATE TABLE files (
+ fileid INTEGER PRIMARY KEY ASC,
+ expname TEXT,
+ ccd INT,
+ band TEXT,
+ name TEXT
+);
+CREATE INDEX files_ccd_idx ON "files" (ccd);
+CREATE INDEX files_expname_idx ON "files" (expname);
+
+CREATE TABLE qa (
+ qaid INTEGER PRIMARY KEY ASC,
+ fileid INT NOT NULL,
+ userid INT NOT NULL,
+ problem INT NOT NULL,
+ x INT,
+ y INT,
+ detail TEXT,
+ timestamp TEXT NOT NULL DEFAULT current_timestamp
+);
+CREATE INDEX qa_fileid_idx ON "qa" (fileid);
+CREATE INDEX qa_problem_idx ON "qa" (problem);
+CREATE INDEX qa_userid_idx ON "qa" (userid);
+
+CREATE TABLE runs (
+ expname text PRIMARY KEY ON CONFLICT IGNORE,
+ runname text
+);

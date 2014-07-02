61df4ff1dd6c762f87fda5968688a6f0dc9cd7a6
Peter Melchior <peter.m.melchior@gmail.com>
2014-07-02 21:30:39 +0000

    separated form from content in ranking and image lists

--- ranking.php
+++ ranking.php
@@ -28,30 +28,7 @@ if (isset($_GET)) {
         check_or_abort($stmt);
         $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
-        if (isset($_GET['output'])) {
-            if ($_GET['output'] == "html") {
-                if ($_GET['short'] == "true")
-                    echo "<table class='table table-condensed table-striped'>";
-                else
-                    echo "<table class='table table-condensed table-striped'><thead><tr><th>Rank</th><th>Username</th><th>Problematic/Total</th><th># Files</th></tr></thead><tbody>\n";
-                $total = null;
-                $counter = 1;
-                foreach ($response as $row) {
-                    if ($total === null)
-                        $total = $row['total_files'];
-                    $width_flagged = (100*$row['flagged_files']/$total) . "%";
-                    $width_total = (100*($row['total_files']-$row['flagged_files'])/$total) . "%";
-                    echo "<tr><td># ". $counter ."</td><td><span class='namecol'>" . $row['username'] . "</span></td>";
-                    echo "<td><div class='ratingcol'><span class='ratingbar bad' style='width:" . $width_flagged ."'></span>";
-                    echo "<span class='ratingbar good' style='left:" . $width_flagged ."; width:" . $width_total ."'></span></div></td>";
-                    echo "<td>" . $row['total_files'] . "</td></tr>\n";
-                    $counter += 1;
-                }
-                echo "</tbody></table>";
-            }
-        }
-        else 
-            echo json_encode($response);
+        echo json_encode($response);
     }
 }
 ?>

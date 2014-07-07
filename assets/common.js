2a666227ae6ca47e6129475f73825d0664892cb2
Peter Melchior <peter.m.melchior@gmail.com>
2014-07-07 04:04:47 +0000

    made Y1A1 default, closes #62

--- assets/common.js
+++ assets/common.js
@@ -2,7 +2,7 @@ var release = null;
 
 function setRelease(release_) {
     if (release_ == null) {
-        release_ = "SVA1";
+        release_ = "Y1A1";
     }
     release = release_;
     $.cookie('default-release', release, {expires: 365});

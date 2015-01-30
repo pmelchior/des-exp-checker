585d456f4ff74edf7a76b4776920ba2ccbf81de5
Peter Melchior <peter.m.melchior@gmail.com>
2015-01-30 02:23:28 -0500

    added heat maps, short option for API

--- api.php
+++ api.php
@@ -7,22 +7,32 @@ if (isset($_GET['problem'])) {
     $codes = getProblemCodes();
     if (array_search($_GET['problem'], array_keys($codes)) !== FALSE) {
         $code = $codes[$_GET['problem']];
-        $stm = $dbh->prepare('SELECT qa.qaid as qa_id, expname, ccd, band, problem, x, y, detail FROM qa JOIN files ON (files.fileid=qa.fileid) WHERE problem=' . $code . ' OR problem=-' . $code . ' ORDER BY expname, ccd ASC');
+        $sql = 'SELECT qa.qaid as qa_id, expname, ccd, band, problem, x, y, detail FROM qa JOIN files ON (files.fileid=qa.fileid)';
+        if (isset($_GET['short']))
+            $sql = 'SELECT qa.qaid as qa_id, ccd, band, problem, x, y FROM qa JOIN files ON (files.fileid=qa.fileid)';
+        $sql .= ' WHERE problem=' . $code . ' OR problem=-' . $code;
+        $sql .= ' ORDER BY expname, ccd ASC';
+        $stm = $dbh->prepare($sql);
         $stm->execute();
         $result = array();
         while($row = $stm->fetch(PDO::FETCH_ASSOC)) {
             $row['qa_id'] = intval($row['qa_id']);
             $row['ccd'] = intval($row['ccd']);
-            $row['false_positive'] = FALSE;
-            if ($row['problem'] < 0)
-                $row['false_positive'] = TRUE;
-            $row['problem'] = $_GET['problem'];
-            $row['release'] = $config['release'];
-            if ($code != 0) { // good exposure don't have locations
+            $row['problem'] = intval($row['problem']);
+            if ($row['problem'] != 0) { // good exposure don't have locations
                 // correct for downsampling of factor 4
-                $row['x'] *= 4;
-                $row['y'] *= 4;
+                $row['x'] = intval($row['x'])*4;
+                $row['y'] = intval($row['y'])*4;
             }
+            if (!isset($_GET['short'])) {
+                $row['false_positive'] = FALSE;
+                if ($row['problem'] < 0)
+                    $row['false_positive'] = TRUE;
+                $row['problem'] = $_GET['problem'];            
+                $row['release'] = $config['release'];
+            }
+            else
+                unset($row['problem']);
             array_push($result, $row);
         }
         echo json_encode($result);

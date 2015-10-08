4a44f878312f7bbf320e17dc5ab0301778b7437a
Peter Melchior <peter.m.melchior@gmail.com>
2015-10-08 21:58:11 +0000

    removed runname from response; image queries now all in getImage.php

--- getImage.php
+++ getImage.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 function download_file($file, $revalidate = False) { // $file = include path 
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
@@ -19,10 +20,36 @@ function download_file($file, $revalidate = False) { // $file = include path
 include("common.php.inc");
 setRelease();
 
-if ($_GET['type'] == "fov") {
-        $path = str_replace("%r", $_GET['runname'], $config['fovpath'][$config['release']]);
+if (isset($_GET['type'])) {
+   $dbh = getDBHandle();
+   // depending on release, need runname or whole other sheband for paths
+   if ($config['release'] == "SVA1" || $config['release'] == "Y1A1") {
+      $sql = 'SELECT runname FROM runs WHERE expname = ?';
+      $stmt = $dbh->prepare($sql);
+      $stmt->bindParam(1, $_GET['expname'], PDO::PARAM_STR, 14);
+      $stmt->execute();
+      $res = check_or_abort($stmt);
+      $row = $res->fetch(PDO::FETCH_ASSOC);
+
+      if ($_GET['type'] == "fov") {
+        $path = str_replace("%r", $row['runname'], $config['fovpath'][$config['release']]);
         $path = str_replace("%e", $_GET['expname'], $path);
-        download_file($path);
+	download_file($path);
+      }
+
+      if ($_GET['type'] == "dm") {
+      	$path = str_replace("%r", $row['runname'], $config['dmpath'][$config['release']]);
+        $path = str_replace("%e", $_GET['expname'], $path);
+	$path = str_replace("%c", sprintf("%02d", $_GET['ccd']), $path);
+	echo $path;
+      }
+   }
+   else {
+     if ($_GET['type'] == "dm")
+       echo "not available yet!";
+     if ($_GET['type'] == "fov")
+       download_file("assets/fov_not_available.png");
+   }
 }
 else {
         $path = $config['fitspath'][$config['release']];

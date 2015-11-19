dd53f5a1309c96b5b8bde8899a8cdf1c8227e308
Peter Melchior <peter.m.melchior@gmail.com>
2015-11-19 21:33:23 +0000

    check for outdated release

--- assets/common.js
+++ assets/common.js
@@ -1,15 +1,27 @@
 var release = null;
 
 function setRelease(release_) {
-  if (release_ == null) {
-    var node = $('#release-switch').find('.release-button').last();
-    release = node.id;
+  if (release_ != null) {
+    // check if release is still present in the list
+    var found = false;
+    $('#release-switch').find('.release-button').each(function() {
+      if (release_ == this.id.split("-").pop()) {
+	found = true;
+      }
+    });
+    if (found)
+      release = release_;
+    else
+      release = null;
   }
-  else
-    release = release_;
+  
+  if (release == null) {
+       var node = $('#release-switch').find('.release-button').last()[0];
+    release = node.id.split("-").pop();
+  }
+
   $.cookie('default-release', release, {expires: 365});
   $('.release_name').html(release);
-
   $('a[class*="release-button"]').on('click', function(evt) {
     setRelease(this.id.split("-").pop());
     window.location.reload(true);

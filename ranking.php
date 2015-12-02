a4875a3571558a39c7215518c88d370ee78a8b9a
Peter Melchior <peter.m.melchior@gmail.com>
2015-12-02 23:32:33 +0000

    removed debug clutter

--- ranking.php
+++ ranking.php
@@ -3,32 +3,21 @@
 include "common.php.inc";
 $dbh = getDBHandle();
 
-if (isset($_GET)) {
-    if (isset($_GET['myrank'])) {
-        $uid = getUIDFromSID($dbh);
-        if ($uid !== FALSE) {
-            echo $uid;
-        }
-    }
-    
-    else {
-        $query = 'SELECT username, total_files, flagged_files FROM submissions JOIN users ON (users.userid=submissions.userid) WHERE release=? AND total_files > 0 ORDER BY total_files DESC';
-        if (isset($_GET['limit'])) {
-            if (is_numeric($_GET['limit'])) {
-                $query .= ' LIMIT ?';
-                $stmt = $dbh->prepare($query);
-                $stmt->bindParam(1, $config['release'], PDO::PARAM_STR, 4);
-                $stmt->bindParam(2, $_GET['limit'], PDO::PARAM_INT);
-            }
-        } else {
-            $stmt = $dbh->prepare($query);
-            $stmt->bindParam(1, $config['release'], PDO::PARAM_STR, 4);
-        }
-        $stmt->execute();
-        check_or_abort($stmt);
-        $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-        echo json_encode($response);
-    }
+$query = 'SELECT username, total_files, flagged_files FROM submissions JOIN users ON (users.userid=submissions.userid) WHERE release=? AND total_files > 0 ORDER BY total_files DESC';
+if (isset($_GET['limit'])) {
+  if (is_numeric($_GET['limit'])) {
+    $query .= ' LIMIT ?';
+    $stmt = $dbh->prepare($query);
+    $stmt->bindParam(1, $config['release'], PDO::PARAM_STR, 4);
+    $stmt->bindParam(2, $_GET['limit'], PDO::PARAM_INT);
+  }
+} else {
+  $stmt = $dbh->prepare($query);
+  $stmt->bindParam(1, $config['release'], PDO::PARAM_STR, 4);
 }
+$stmt->execute();
+check_or_abort($stmt);
+$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($response);
 ?>
